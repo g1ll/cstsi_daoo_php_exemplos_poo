be8fa1d1575d7a6a53eb0111745e284908f4a9ca
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-07-14 21:15:12 -0300

    refact: update the use of class Relatorio

--- aula_02_exemplos_abstract_interface_trait_try-catch/exemplo-04-abstract-class.php
+++ aula_02_exemplos_abstract_interface_trait_try-catch/exemplo-04-abstract-class.php
@@ -8,5 +8,7 @@ use classes\logs\Relatorio;
 $medico = new Medico("Paulo Paixão",12345,"Fisiologista");
 $jogador = new Atleta("Pedro Geromel", 1.9, 84);
 
-$relatorio = new Relatorio([$medico, $jogador]);
+$relatorio = new Relatorio;
+$relatorio->add($medico);
+$relatorio->add($jogador);
 $relatorio->imprime();
