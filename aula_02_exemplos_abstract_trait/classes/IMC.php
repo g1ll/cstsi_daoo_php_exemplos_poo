48f73cfce81762350664e383da55296c03c35eee
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-07-15 11:38:33 -0300

    feat: create trait IMC.

---
+++ aula_02_exemplos_abstract_trait/classes/IMC.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace classes;
+
+trait IMC
+{
+	protected $imc;
+	
+	public function calcImc(){
+        if ($this->peso && $this->altura) {
+            $this->imc = $this->peso/$this->altura**2;
+        }else{
+			echo "Erro, informe o peso e a altura primeiro!";
+		}
+	}
+
+}
