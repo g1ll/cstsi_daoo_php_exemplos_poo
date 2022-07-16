6acbab0afdb9e10af0518cba10ab5541eff67dfe
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-07-15 23:58:42 -0300

    refact: comment path fix example.

--- aula_01_exemplo_imc/exemplos/exemplo-04-namespace.php
+++ aula_01_exemplo_imc/exemplos/exemplo-04-namespace.php
@@ -1,7 +1,7 @@
 <?php 
 spl_autoload_register(function ($class_name) {
 	$path = implode("/",explode('\\',$class_name));
-    require_once "../".$path . '.php';
+    require_once "../".$path . '.php';//fix folder path
 });
 
 use classes\Atleta;
