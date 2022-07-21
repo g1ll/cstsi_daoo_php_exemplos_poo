9af06369c24fc1c6e3340317da0a7386fd754ceb
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-07-21 20:18:14 -0300

    refact: move file to root dir of the system

--- aula_04_mvc_pdo/app/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type:application/json;charset=utf-8'");
-require '../vendor/autoload.php';
-
-use app\controller\Produto;
-
-$controllerProduto = new Produto();
-echo $controllerProduto->index();
