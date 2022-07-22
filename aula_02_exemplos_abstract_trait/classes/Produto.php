2ce1f140aa15204341ad380d1fc71816d235833f
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-07-22 16:01:41 -0300

    feat: create Produto controller.

---
+++ aula_02_exemplos_abstract_trait/classes/Produto.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\controller;
+
+use app\model\ProdutoDao;
+
+class Produto extends Controller
+{
+
+	public function __construct()
+	{
+		header("Content-Type:application/json;charset=utf-8'");
+		$this->model = new ProdutoDao();
+	}
+
+	public function index()
+	{
+		echo json_encode($this->model->read());
+	}
+
+	public function show($id)
+	{
+		$produto = $this->model->show($id);
+		if ($produto) {
+			$json = ['produto' => $produto];
+		} else {
+			$json = ['Erro' => "Produto não encontrado"];
+			header('HTTP/1.0 404 Not Found');
+		}
+		echo json_encode($json);
+	}
+}
